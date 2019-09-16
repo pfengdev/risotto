504bb52e357923dace374a203483612cdc1f718f
pfengdev <pfengdev@gmail.com>
2019-09-15 21:41:45 -0700

    Adding page to view lyrics for each song

--- spotify/urls.py
+++ spotify/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('genius', views.genius, name = 'genius'),
     path('playlists', views.playlists_list, name = 'playlists_list'),
     path('playlists/<str:playlist_id>/songs', views.playlists_detail, name = 'playlists_detail'),
-    path('songs/<str:song_id>', views.song_lyrics, name = 'song_lyrics')
+    path('songs/<str:search_artist>/<str:song_name>', views.song_lyrics, name = 'song_lyrics')
 ]
--- spotify/views.py
+++ spotify/views.py
@@ -2,10 +2,15 @@ from django.http import HttpResponse
 from django.template import loader
 import spotipy
 import spotipy.util as util
+import lyricsgenius
 import logging
 
 def index(request):
     template = loader.get_template('spotify/index.html')
+    return HttpResponse(template.render({}, request))
+
+def genius(request):
+    template = loader.get_template('spotify/genius.html')
     return HttpResponse(template.render({}, request))
 
 def playlists_list(request):
@@ -25,6 +30,8 @@ def playlists_detail(request, playlist_id):
     songs_response = spotify_client.user_playlist_tracks(username, playlist_id)
     songs = []
     for item in songs_response['items']:
+        song = item['track']
+        song['search_artist'] = song['artists'][0]['name']
         songs.append(item['track'])
     template = loader.get_template('spotify/playlists_detail.html')
     context = {
@@ -32,8 +39,27 @@ def playlists_detail(request, playlist_id):
     }
     return HttpResponse(template.render(context, request))
 
+def song_lyrics(request, search_artist, song_name):
+    username = request.user.username
+    genius_client = get_genius_client(request)
+    song = genius_client.search_song(song_name, search_artist)
+    song_lyrics_full_text = song.lyrics
+    song_lines = song_lyrics_full_text.split('\n')
+    template = loader.get_template('spotify/song_lyrics.html')
+    context = {
+        'song_name': song_name,
+        'song_lines': song_lines
+    }
+    return HttpResponse(template.render(context, request))
+
 def get_spotify_client(request):
     username = request.user.username
-    social = request.user.social_auth.get(provider='spotify')
-    token = social.extra_data['access_token']
+    spotify_social = request.user.social_auth.get(provider='spotify')
+    token = spotify_social.extra_data['access_token']
     return spotipy.Spotify(auth = token)
+
+def get_genius_client(request):
+    username = request.user.username
+    genius_social = request.user.social_auth.get(provider='genius')
+    token = genius_social.extra_data['access_token']
+    return lyricsgenius.Genius(client_access_token=token)
